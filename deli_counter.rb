b3e4dbe928c20775104223856b8d4e4defcbcda5
Ann Kim <annkim822@gmail.com>
2018-06-20 19:13:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,7 @@ def line(katz_deli)
     line_length.each.with_index(0) do |name, index|
     line_method_array.push("#{index}. #{name}")
   end
+  
 end
  
     puts "The line is: #{line_method_array.join(" ")}"

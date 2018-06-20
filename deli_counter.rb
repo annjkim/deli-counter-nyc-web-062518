f9c8976b5b87fa11c758e26059301c3d97df793f
Ann Kim <annkim822@gmail.com>
2018-06-20 19:11:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,15 @@ def line(katz_deli)
   end
 end
   
+  def line_method(numinline)
+  line_method_array = []
+  if numinline.length == 0
+    puts "The line is currently empty."
+  else
+    line_method_array.each.with_index(0) do |name, index|
+      line_method_array.push("#{index}. #{name}")
+      # push method will push the name into the index 
+    end
+    puts "The line is: #{line_method_array.join(" ")}"
+  end
+end

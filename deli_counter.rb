8460926f1a6130684009e96b3d7a9f016f54174b
Ann Kim <annkim822@gmail.com>
2018-06-20 19:12:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,23 +1,13 @@
 def line(katz_deli)
+  line_length = []
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    message = "The line is currently:"
-    
-    katz_deli.each_with_index do |value, index|
-    message += " #{index.to_i+1}. #{value}"
+    line_length.each.with_index(0) do |name, index|
+    line_method_array.push("#{index}. #{name}")
   end
 end
-  
-  def line_method(numinline)
-  line_method_array = []
-  if numinline.length == 0
-    puts "The line is currently empty."
-  else
-    line_method_array.each.with_index(0) do |name, index|
-      line_method_array.push("#{index}. #{name}")
-      # push method will push the name into the index 
-    end
+ 
     puts "The line is: #{line_method_array.join(" ")}"
   end
 end
